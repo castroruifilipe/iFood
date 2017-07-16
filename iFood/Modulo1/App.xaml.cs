2a5ddd6643da4b37249487ee0e0e3bbe3cfc6c62
Rui Leite <ruicastro@outlook.com>
2017-07-16 18:35:43 +0100

    Tela inicial e tabed pages

---
+++ iFood/Modulo1/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Modulo1.Pages;
+
+namespace Modulo1
+{
+    public partial class App : Application
+    {
+        public static bool UseMockDataStore = true;
+        public static string BackendUrl = "https://localhost:5000";
+
+        public static IDictionary<string, string> LoginParameters => null;
+
+        public App() {
+            MainPage = new MenuPage();
+        }
+
+        public static void SetMainPage()
+        {
+            if (!UseMockDataStore && !Settings.IsLoggedIn)
+            {
+                Current.MainPage = new NavigationPage(new LoginPage())
+                {
+                    BarBackgroundColor = (Color)Current.Resources["Primary"],
+                    BarTextColor = Color.White
+                };
+            }
+            else
+            {
+                GoToMainPage();
+            }
+        }
+
+        public static void GoToMainPage()
+        {
+            Current.MainPage = new TabbedPage
+            {
+                Children = {
+                    new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Browse",
+                        Icon = Device.OnPlatform("tab_feed.png", null, null)
+                    },
+                    new NavigationPage(new AboutPage())
+                    {
+                        Title = "About",
+                        Icon = Device.OnPlatform("tab_about.png", null, null)
+                    },
+                }
+            };
+        }
+    }
+}
---
+++ iFood/Modulo1/Pages/Garcons/GarconsNewPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+
+namespace Modulo1.Pages.Garcons {
+
+	public class GarconsNewPage : ContentPage {
+
+
+		public GarconsNewPage() {
+			
+		}
+    }
+}
